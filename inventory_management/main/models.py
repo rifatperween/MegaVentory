c28a80a416dcba92e78d362b783aec9220be57e9
rifatperween <116188576+rifatperween@users.noreply.github.com>
2023-01-29 08:55:56 +0530

    Delete inventory_management directory

--- inventory_management/main/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class warehouseItem(models.Model):
-    name = models.CharField(max_length=40)
-    value= models.IntegerField()
-    hasShipped = models.BooleanField(default=False)
-    def __str__(self):
-        return self.name
